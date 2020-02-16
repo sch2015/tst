ae1974ede7dc01b90ad9dc2cb66cad31c99d7bd9
lhk <564352527@qq.com>
2020-02-16 15:36:21 +0800

    new1

--- grpctest/Program.cs
+++ grpctest/Program.cs
@@ -21,7 +21,7 @@ namespace grpctest
             Console.ReadKey();
             Console.WriteLine("Hello World!");
 
-            Console.WriteLine("d55555556d");
+            Console.WriteLine("new");
         }
     }
 }
